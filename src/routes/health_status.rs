67737a96f191f543dd693970949d8142296f6d26
Rafael Fiume <rafael.fiume@yahoo.it>
2023-12-05 06:07:44 +0000

    Pass async function as argument to another function

--- src/routes/health_status.rs
+++ src/routes/health_status.rs
@@ -1,3 +1,5 @@
+use std::future::Future;
+
 use crate::health_check::service_status::{Dependency, DependencyStatus, ServiceStatus, Status};
 use crate::health_check::version::{Environment, Version, VersionLoadError};
 use warp::reject::{self, Rejection};
@@ -5,23 +7,32 @@ use warp::reply::Reply;
 use warp::Filter;
 
 pub fn routes() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-    ping().or(health_check())
+    ping().or(health_check(|| do_check_health()))
 }
 
 fn ping() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
     warp::path("ping").map(|| warp::reply::json(&"pong"))
 }
 
-fn health_check() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-    warp::path("status").and_then(|| async {
-        match check_health().await {
-            Ok(service_health) => Ok(warp::reply::json(&service_health)),
-            Err(e) => Err(reject::custom(e)),
+fn health_check<F, S>(
+    check_health: F,
+) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone
+where
+    F: Fn() -> S + Clone + Send + Sync,
+    S: Future<Output = Result<ServiceStatus, VersionLoadError>> + Send,
+{
+    warp::path("status").and_then(move || {
+        let fnn = check_health.clone();
+        async move {
+            match fnn().await {
+                Ok(service_health) => Ok(warp::reply::json(&service_health)),
+                Err(e) => Err(reject::custom(e)),
+            }
         }
     })
 }
 
-async fn check_health() -> Result<ServiceStatus, VersionLoadError> {
+async fn do_check_health() -> Result<ServiceStatus, VersionLoadError> {
     // TODO Hardcoded
     let env = Environment::new("dev".to_string());
     let version = Version::current_version(env, &"rustic.version".to_string())?;
