773df9c4f324acde47f87798519c597d0dc798b3
Rafael Fiume <rafael.fiume@yahoo.it>
2024-08-24 17:07:45 +0100

    Status endpoint checks db dependency

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use health_check::{
     RusticSketchHealthChecker,
 };
 use routes::health_status;
+use store::postgres::{DatabaseConfig, PostgresStore};
 use std::sync::Arc;
 use warp::Filter;
 
@@ -18,14 +19,25 @@ pub async fn run() {
         Environment::new("dev".to_string()), // TODO Hardcoded for now
         "rustic.version".to_string(),
     );
+    // TODO DB config hardcoded for now
+    let db_config = DatabaseConfig::new(
+        "rustic-database".to_string(),
+        5432,
+        "rustic-sketch".to_string(),
+        "rustic-sketch.dev".to_string(),
+        "rustic-sketch.pw".to_string(),
+        5,
+    );
+    let store = PostgresStore::new(db_config).await.expect("Failed to instantiate PostgresStore");
     let health_checker = RusticSketchHealthChecker::new(
         Box::new(versioned),
-        vec![], // TODO empty for now
+        vec![Box::new(store)],
     );
     let routes = health_status::routes(Arc::new(health_checker))
         .or(hello_route)
         // TODO any origins for now
         .with(warp::cors().allow_any_origin());
 
+    // TODO Port hardcoded for now
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await
 }
